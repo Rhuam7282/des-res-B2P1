d31a5432c3ea80ff9af288edabdd521bbbca55e5
Rhuam7282 <rhuamramos7282@gmail.com>
2025-05-20 08:11:08 -0300

    Add files via upload

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -6,6 +6,15 @@ import queimado from '../assets/images/queimado.jpg';
 import agua from '../assets/images/agua.jpg';
 import cocada from '../assets/images/cocada.jpg';
 import bala from '../assets/images/bala.jpg';
+import vidro from '../assets/images/vidro.jpg';
+import torta from '../assets/images/torta.jpg';
+import sequilho from '../assets/images/sequilho.jpg';
+import pudim from '../assets/images/pudim.jpg';
+import manjar from '../assets/images/manjar.jpg';
+import batida from '../assets/images/batida.jpg';
+import assado from '../assets/images/assado.jpg';
+import bolo from '../assets/images/bolo.jpg';
+import chips from '../assets/images/chips.jpg';
 export const cardapio = {
   entradas: [
     { 
@@ -20,6 +29,27 @@ export const cardapio = {
       nome: "Coco em pó",
       descricao: "Nossa entrada de coco em pó tem coco que é em pó",
       preco: "R$0,75banana",
+      categoria: "entradas"
+    },
+    { 
+      imagem: chips,
+      nome: "Chips de coco",
+      descricao: "Nossa entrada de chips de coco tem coco que é em chips",
+      preco: "R$1banana",
+      categoria: "entradas"
+    },
+    { 
+      imagem: batida,
+      nome: "Batida de coco",
+      descricao: "Nossa entrada de batida de coco tem coco que é batido",
+      preco: "R$1,5banana",
+      categoria: "entradas"
+    },
+    { 
+      imagem: sequilho,
+      nome: "Sequilho de coco",
+      descricao: "Nossa entrada de sequilho de coco tem sequilho que é de coco",
+      preco: "R$0,5banana",
       categoria: "entradas"
     }
   ],
@@ -44,6 +74,20 @@ export const cardapio = {
       descricao: "Nosso prato principal de coco queimado tem coco que é queimado",
       preco: "R$2bananas",
       categoria: "pratos"
+    },
+    { 
+      imagem: assado,
+      nome: "Cocada assado",
+      descricao: "Nosso prato principal de cocada assada tem coco que é assado",
+      preco: "R$1,5bananas",
+      categoria: "pratos"
+    },
+    { 
+      imagem: torta,
+      nome: "Torta de coco",
+      descricao: "Nosso prato principal de torta de coco tem torta que é de coco",
+      preco: "R$3bananas",
+      categoria: "pratos"
     }
   ],
   sobremesas: [
@@ -65,6 +109,34 @@ export const cardapio = {
       imagem: bala,
       nome: "Bala de coco",
       descricao: "Nossa sobremesa de bala de coco é uma porção de balas de coco",
+      preco: "R$1bananas",
+      categoria: "sobremesas"
+    },
+    { 
+      imagem: bolo,
+      nome: "Bolo de coco",
+      descricao: "Nossa sobremesa de bolo de coco é um bolo que é de coco",
+      preco: "R$2bananas",
+      categoria: "sobremesas"
+    },
+    { 
+      imagem: manjar,
+      nome: "Manjar de coco",
+      descricao: "Nossa sobremesa de manjar de coco é uma porção de manjar de coco",
+      preco: "R$1,5bananas",
+      categoria: "sobremesas"
+    },
+    { 
+      imagem: pudim,
+      nome: "Pudim de coco",
+      descricao: "Nossa sobremesa de pudim de coco é uma porção de pudim de coco",
+      preco: "R$1,5bananas",
+      categoria: "sobremesas"
+    },
+    { 
+      imagem: vidro,
+      nome: "Vidro de coco",
+      descricao: "Nossa sobremesa de vidro de coco é uma porção de balinhas de vidro, de coco",
       preco: "R$1bananas",
       categoria: "sobremesas"
     }
