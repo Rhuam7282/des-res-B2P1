95ee2c601a70c65283b0e942ccab54c53e7978e7
Rhuam7282 <rhuamramos7282@gmail.com>
2025-05-27 09:11:17 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}", // Focado em arquivos JS e JSX conforme solicitado
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
